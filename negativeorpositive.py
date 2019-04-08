87f56c1cda9b8952f891ca3814fe1e86ea24ff99
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-04-07 20:41:10 -0400

    Update negativeorpositive.py

--- negativeorpositive.py
+++ negativeorpositive.py
@@ -1,7 +1,14 @@
+# Created by: Aden Rao
+# Created on: April 7, 2019
+# This program lets a user enter a number and it tells them if the number is positive or negative.
+
+# Function for importing math
 import math
 
+# Input that lets teh user enter a number and tells them to enter a number
 integer = int(input("Type a number: "))
 
+# If statement for determining if the number is negative or postitive
 if integer > 0:
 	print("Positive integer")
 	print()
